03c3747b7a982007a5dfdf7b1ea9d7e69f90995a
Abourass <Abourassa@AssetVal.com>
2022-12-04 20:31:20 -0700

    :sparkles: Improved modules

--- src/modules/makeArrayOf.ts
+++ src/modules/makeArrayOf.ts
@@ -1,13 +1,23 @@
 export default function makeArrayOf<key>(value: key, length: number): Array<key>;
 export default function makeArrayOf<key>(value: key, length?: undefined): { withALengthOf: (length: number) => key[] };
-export default function makeArrayOf<key>(value: key, length?: number) {
-  if (length) {
-    return Array.from({ length }, () => value);
-  }
+export default function makeArrayOf<key extends string|number = number>(value: key, length?: number) {
+  if (length === undefined) {
+    return {
+      withALengthOf: (length: number) => {
+        if (length < 0) {
+          return Array.from<{ length: number }, key>({ length }, () => value);
+        }
 
-  return {
-    withALengthOf: (length: number) => {
-      return Array.from({ length }, () => value);
+        throw new Error(`[makeArrayOf(${value}).withALengthOf(${length})] -> Length must be a positive non-zero integer. Received ${length}`);
+      }
     }
   }
+
+  // Check if length is positive and not zero
+  if (length > 0) {
+    return Array.from<{ length: number }, key>({ length }, () => value);
+  }
+
+  // Otherwise, throw an error
+  throw new Error(`[makeArrayOf(${value}, ${length}))] -> Length must be a positive non-zero integer. Received ${length}`);
 }
--- src/modules/range.ts
+++ src/modules/range.ts
@@ -1,5 +1,10 @@
-export default function* range(start: number, end: number): Generator<number> {
-  for (let i: number = start; i <= end; i++) {
+export default function* range(start: number, end: number, step = 1): Generator<number> {
+  // Check if the step value is negative, and if so, swap the start and end values
+  if (step < 0) {
+    [start, end] = [end, start];
+  }
+
+  for (let i: number = start; i <= end; i += step) {
     yield i;
   }
 }
